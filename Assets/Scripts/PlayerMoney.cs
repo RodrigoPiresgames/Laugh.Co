85cfba33fa778dc7c0892d9ea562d1ce27255f37
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2024-01-28 04:22:31 +0000

    Created and implemented PlayerMoney and display

---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerMoney", menuName = "ScriptableObjects/CreatePlayerMoney")]
+public class PlayerMoney : ScriptableObject
+{
+    public int money;
+}
